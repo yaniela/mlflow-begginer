9c2975b07d6d23f707f0aa04a029488698178b6b
Yaniela <yanielajanis@gmail.com>
2024-02-20 16:14:22 -0300

    deleted folders

--- compute_bmi.py
+++ compute_bmi.py
@@ -1,4 +1,5 @@
 import mlflow
+from mlflow.tracking import MlflowClient
 
 def calculate_bmi(height, weight):
     """ Function to calculate the BMI
@@ -9,21 +10,40 @@ def calculate_bmi(height, weight):
     """
     return weight / (height/100)**2
 
+def initialize_mlflow():
+    # Initialize MLflow
+    experiment_name = "compute_bmi_experiments"
+    
 
+    # Provide uri and connect to your tracking server
+    mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+
+    # Initialize MLflow client
+    client = MlflowClient()
+
+    # If experiment doesn't exist then it will create new
+    # else it will take the experiment id and will use to to run the experiments
+    try:
+        # Create experiment 
+        experiment_id = client.create_experiment(experiment_name)
+        return experiment_id
+    except:
+        # Get the experiment id if it already exists
+        experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
+        return experiment_id
+ 
 
 if __name__ == "__main__":
-
     names = ['Bob', 'Lital', 'Simona']
     ages = [18, 32, 44]
     heights = [150, 165, 172]
-    weights = [55, 80, 100]
+    weights = [55, 80, 100]  
 
-
+    experiment_id = initialize_mlflow()
     for i, (height, weight) in enumerate(zip(heights, weights)):   
         
-        with mlflow.start_run(run_name = names[i]):
-            print(names[i], ages[i], height, weight, calculate_bmi(height, weight))
-            
+        with mlflow.start_run(experiment_id=experiment_id, run_name = names[i]):
+            print(names[i], ages[i], height, weight, calculate_bmi(height, weight))            
             mlflow.set_tag('age', ages[i])   
             mlflow.log_param('height', height)
             mlflow.log_param('weight', weight)
